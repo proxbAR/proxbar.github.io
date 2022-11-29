2f025ddf821e525cabe032c679e5594008c44010
proxbar <anandravindran123@gmail.com>
2022-11-30 00:55:51 +0530

    Modified the message in javascript/main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.first',{
-  strings:["<h1>Hello.</h1>","<h1>I'm Anand.</h1>","<h1>I'm a <strong>Web Developer</strong>,</h1>","<h1>And <strong>Graphic Designer.</strong></h1>","<h1>Welcome to my website!</h1>"],
+  strings:["<h1>Hello.</h1>","<h1>I'm Anand.</h1>","<h1>I'm a <strong>Cloud Engineer</strong>,</h1>","<h1><strong>Web Developer</strong>,</h1>","<h1>And <strong>Graphic Designer.</strong></h1>","<h1>Welcome to my website!</h1>"],
   backSpeed: 50,
   typeSpeed: 50,
   loop: true
